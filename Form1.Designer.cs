e6359d5439c005aedbfd00d68e3cff65d27243ec
Jonathan J <contact@jja-dev.fr>
2023-12-14 21:01:16 +0100

    Ta clicker lol

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnNoClick = new System.Windows.Forms.Button();
+            this.displayMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNoClick
+            // 
+            this.btnNoClick.Location = new System.Drawing.Point(82, 96);
+            this.btnNoClick.Name = "btnNoClick";
+            this.btnNoClick.Size = new System.Drawing.Size(75, 23);
+            this.btnNoClick.TabIndex = 0;
+            this.btnNoClick.Text = "Click pas !!!";
+            this.btnNoClick.UseVisualStyleBackColor = true;
+            this.btnNoClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayMsg
+            // 
+            this.displayMsg.AutoSize = true;
+            this.displayMsg.Location = new System.Drawing.Point(359, 106);
+            this.displayMsg.Name = "displayMsg";
+            this.displayMsg.Size = new System.Drawing.Size(35, 13);
+            this.displayMsg.TabIndex = 1;
+            this.displayMsg.Text = "label1";
+            this.displayMsg.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 449);
+            this.Controls.Add(this.displayMsg);
+            this.Controls.Add(this.btnNoClick);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnNoClick;
+        private System.Windows.Forms.Label displayMsg;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace LearnWinForms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            displayMsg.Text = "Ta clicker !!!!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
